a3f265b24af423b2fad5df1afaec9116e091c1ba
Nicolas <NIKO.ALTERS@GMAIL.COM>
2026-01-06 16:55:34 -0300

    actualizando stock1

--- models/Product.js
+++ models/Product.js
@@ -12,10 +12,24 @@ const reviewSchema = mongoose.Schema({
 }, { timestamps: true });
 
 const productSchema = mongoose.Schema({
+  // Agregamos la referencia al Admin que creó el producto (Buena práctica)
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    required: true,
+    ref: 'User',
+  },
   nombre: { type: String, required: true },
   precio: { type: Number, required: true },
   categoria: { type: String, required: true },
   imagen: { type: String, required: true },
+  
+  // --- CAMPOS QUE FALTABAN Y AGREGAMOS AHORA ---
+  marca: { type: String, required: true, default: 'Genérica' },
+  descripcion: { type: String, required: false, default: '' },
+  
+  // ESTE ES EL QUE ARREGLA TU PROBLEMA DE STOCK 👇
+  countInStock: { type: Number, required: true, default: 0 },
+
   reviews: [reviewSchema],
   rating: { type: Number, required: true, default: 0 },
   numReviews: { type: Number, required: true, default: 0 },
