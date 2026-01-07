db351923e1644ea2b8abd78a16a13e953f8bcd2a
Nicolas <NIKO.ALTERS@GMAIL.COM>
2026-01-07 01:30:00 -0300

    realizando ajustes1

--- routes/orderRoutes.js
+++ routes/orderRoutes.js
@@ -4,8 +4,9 @@ import {
   addOrderItems,
   getOrderById,
   getMyOrders,
-  getOrders,          // <--- Importar
-  updateOrderToPaid   // <--- Importar
+  getOrders,
+  updateOrderToPaid,
+  updateOrderToDelivered // <--- 1. IMPORTAR LA NUEVA FUNCIÓN
 } from '../controllers/orderController.js';
 
 // Importamos 'protect' y 'admin' desde tu archivo authMiddleware.js
@@ -13,7 +14,7 @@ import { protect, admin } from '../middleware/authMiddleware.js';
 
 // 1. RUTA RAIZ: /api/orders
 // POST: Cliente crea pedido
-// GET: Admin ve TODOS los pedidos (protect + admin)
+// GET: Admin ve TODOS los pedidos
 router.route('/')
     .post(protect, addOrderItems)
     .get(protect, admin, getOrders); 
@@ -26,5 +27,8 @@ router.route('/:id').get(protect, getOrderById);
 
 // 4. RUTA PAGAR (Admin aprueba el pago)
 router.route('/:id/pay').put(protect, admin, updateOrderToPaid);
+
+// 5. RUTA ENVIAR / ENTREGA (Admin marca como enviado) 👇 NUEVA
+router.route('/:id/deliver').put(protect, admin, updateOrderToDelivered);
 
 export default router;
