c6a72c9d7625ef3e15a4563bb3b21186bcb72a0a
Nicolas <NIKO.ALTERS@GMAIL.COM>
2026-01-07 18:34:40 -0300

    Update User model and controller structure

--- controllers/orderController.js
+++ controllers/orderController.js
@@ -1,6 +1,6 @@
 import asyncHandler from 'express-async-handler';
 import Order from '../models/orderModel.js';
-import Product from '../models/productModel.js'; // <--- 1. IMPORTANTE: Importar Producto
+import Product from '../models/Product.js';
 
 // @desc    Crear un nuevo pedido
 // @route   POST /api/orders
